7a87ea9463f1da5eefaa645199f49abac80348f8
Qingtian <qingtianyu5@gmail.com>
2024-07-14 23:24:29 -0400

    first commit

---
+++ movies/src/test/java/dev/royyu/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.royyu.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
